1d6ae188b39d14d536fa50e15b510ea5fc131bfa
Adriane Augusto <adrianega@hotmail.com>
2023-06-01 23:32:56 -0300

    escrita do código para primeira entrega

---
+++ script.js
@@ -0,0 +1,14 @@
+var numbersQuestion = alert("A seguir, digite dois números para saber a média entre eles!");
+
+var firstNumber = prompt("Primeiro número: ");
+
+var secundNumber = prompt("Segundo número: ");
+
+let firstNumberInt = parseInt(firstNumber);
+
+let secundNumberInt = parseInt(secundNumber);
+
+var Avarage = (firstNumberInt + secundNumberInt)/2;
+
+alert("A média entre os números informados é: " + Avarage); 
+

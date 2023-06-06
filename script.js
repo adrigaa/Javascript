031fe8cf5e87c222fb782fa5cd35351148dde1ea
Adriane Augusto <123701194+adrigaa@users.noreply.github.com>
2023-06-06 20:40:53 -0300

    entrega 2

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-var numbersQuestion = alert("A seguir, digite dois números para saber a média entre eles!");
+let age;
 
-var firstNumber = prompt("Primeiro número: ");
+function yourAge() {
+    age = parseInt(prompt("Digite o ano em que nasceu"));
+}
 
-var secundNumber = prompt("Segundo número: ");
+yourAge();
 
-let firstNumberInt = parseInt(firstNumber);
+function whatsGeneration() {
+    if (age >= 1946 & age <= 1964) {
+        alert ("Você pertence à geração Baby Boomers!");
+    } else if (age >= 1965 & age <= 1980) {
+        alert ("Você pertence à geração X!");
+    } else if (age >= 1981 & age <= 1996) {
+        alert ("Você pertence à geração Y (Millennials)!");
+    } else if (age >= 1997 & age <= 2010) {
+        alert ("Você pertence à geração Z!");
+    } else if (age > 2010) {
+        alert ("Você pertence à geração Alfa!")
+    }
+}
 
-let secundNumberInt = parseInt(secundNumber);
-
-var Avarage = (firstNumberInt + secundNumberInt)/2;
-
-alert("A média entre os números informados é: " + Avarage); 
+whatsGeneration();
 

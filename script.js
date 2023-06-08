bb410197a17e8a04112749e712f80acc0f172be4
Adriane Augusto <123701194+adrigaa@users.noreply.github.com>
2023-06-08 15:10:51 -0300

    entrega 3

--- script.js
+++ script.js
@@ -1,24 +1,8 @@
-let age;
+var number = prompt("Insira um número:");
+var total = 0;
 
-function yourAge() {
-    age = parseInt(prompt("Digite o ano em que nasceu"));
+for (var i = 1; i <= number; i++) {
+  total += i;
 }
 
-yourAge();
-
-function whatsGeneration() {
-    if (age >= 1946 & age <= 1964) {
-        alert ("Você pertence à geração Baby Boomers!");
-    } else if (age >= 1965 & age <= 1980) {
-        alert ("Você pertence à geração X!");
-    } else if (age >= 1981 & age <= 1996) {
-        alert ("Você pertence à geração Y (Millennials)!");
-    } else if (age >= 1997 & age <= 2010) {
-        alert ("Você pertence à geração Z!");
-    } else if (age > 2010) {
-        alert ("Você pertence à geração Alfa!")
-    }
-}
-
-whatsGeneration();
-
+alert("A soma de todos os números de 1 até " + number + " é: " + total);

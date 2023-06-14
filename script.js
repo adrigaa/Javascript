f3e83137571eaaf533d800768004a5fa3975841b
Adriane Augusto <123701194+adrigaa@users.noreply.github.com>
2023-06-13 23:22:01 -0300

    entrega4

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-var number = prompt("Insira um número:");
-var total = 0;
+function calcularValorLiquido() {
+    var valorBruto = prompt("Digite o valor bruto:");
 
-for (var i = 1; i <= number; i++) {
-  total += i;
+    if (valorBruto !== null && valorBruto !== "") {
+        var valor = parseFloat(valorBruto);
+        if (!isNaN(valor)) {
+            var valorLiquido = calcularValorComImpostos(valor);
+
+            alert("O valor líquido é: " + valorLiquido.toFixed(2));
+        } else {
+            alert("Digite um valor válido!");
+        }
+    }
 }
 
-alert("A soma de todos os números de 1 até " + number + " é: " + total);
+function calcularValorComImpostos(valorBruto) {
+    var impostoPCC = 0.0465; 
+    var valorLiquido = valorBruto * (1 - impostoPCC);
+    return valorLiquido;
+}

281fd978d1f88a1a56a3a5e3c3407e9d2cc445bc
Adriane Augusto <123701194+adrigaa@users.noreply.github.com>
2023-06-22 20:16:50 -0300

    entrega 5

--- script.js
+++ script.js
@@ -1,20 +1,68 @@
-function calcularValorLiquido() {
-    var valorBruto = prompt("Digite o valor bruto:");
+var produtos = [];
 
-    if (valorBruto !== null && valorBruto !== "") {
-        var valor = parseFloat(valorBruto);
+function Produto(nome, valorBruto) {
+    this.nome = nome;
+    this.valorBruto = valorBruto;
+    this.valorLiquido = calcularValorComImpostos(valorBruto);
+}
+
+function adicionarProduto() {
+    var nomeProduto = document.getElementById("nome-produto").value;
+    var valorBrutoProduto = document.getElementById("valor-bruto-produto").value;
+
+    if (nomeProduto !== "" && valorBrutoProduto !== "") {
+        var valor = parseFloat(valorBrutoProduto);
         if (!isNaN(valor)) {
-            var valorLiquido = calcularValorComImpostos(valor);
-
-            alert("O valor líquido é: " + valorLiquido.toFixed(2));
+            var produto = new Produto(nomeProduto, valor);
+            produtos.push(produto);
+            exibirProdutos();
+            limparCampos();
         } else {
             alert("Digite um valor válido!");
         }
     }
 }
 
+function calcularValorLiquidoTotal() {
+    if (produtos.length === 0) {
+        alert("A lista de produtos está vazia!");
+        return;
+    }
+
+    var valorLiquidoTotal = 0;
+
+    for (var i = 0; i < produtos.length; i++) {
+        var produto = produtos[i];
+        valorLiquidoTotal += produto.valorLiquido;
+    }
+
+    exibirResultado(valorLiquidoTotal);
+}
+
 function calcularValorComImpostos(valorBruto) {
-    var impostoPCC = 0.0465; 
+    var impostoPCC = 0.2;
     var valorLiquido = valorBruto * (1 - impostoPCC);
     return valorLiquido;
+}
+
+function exibirProdutos() {
+    var listaProdutos = document.getElementById("lista-produtos");
+    listaProdutos.innerHTML = "";
+
+    for (var i = 0; i < produtos.length; i++) {
+        var produto = produtos[i];
+        var itemLista = document.createElement("li");
+        itemLista.textContent = produto.nome + ": R$ " + produto.valorBruto.toFixed(2) + " (Valor líquido: R$ " + produto.valorLiquido.toFixed(2) + ")";
+        listaProdutos.appendChild(itemLista);
+    }
+}
+
+function exibirResultado(valorLiquidoTotal) {
+    var resultado = document.getElementById("resultado");
+    resultado.textContent = "Valor líquido total: R$ " + valorLiquidoTotal.toFixed(2);
+}
+
+function limparCampos() {
+    document.getElementById("nome-produto").value = "";
+    document.getElementById("valor-bruto-produto").value = "";
 }
